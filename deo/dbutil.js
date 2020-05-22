67520bdda2e81034680201f373ff76210aa0600c
fuchangda <1014695808@qq.com>
2020-05-22 09:52:57 +0800

    myBlog522

--- deo/dbutil.js
+++ deo/dbutil.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 function createConnection () {
     var connection = mysql.createConnection({
-        host: '192.168.2.101',
+        host: '192.168.56.1',//  这里如是是用虚拟机那就改为虚拟Ip地址，window命令行按ipconfig查看
         post: '3306',
         user: 'root',
         password: 'asd147...',
